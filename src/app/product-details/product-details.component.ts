eafd8631fca4a83628173e30b0d024b9eefc70ef
sandurkov <sandurkov@yahoo.de>
2020-03-05 10:09:54 +0100

    3 files modified, 2 added 05.03. 10:09

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -8,12 +8,16 @@ import { products } from '../products';
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
+
 export class ProductDetailsComponent implements OnInit {
   product;
-  
+
   constructor(private route: ActivatedRoute,) { }
 
   ngOnInit() {
+   this.route.paramMap.subscribe(params => {
+     this.product = products[+params.get('productId')];
+   })
   }
 
 }
